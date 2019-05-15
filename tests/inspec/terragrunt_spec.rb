0f7628be0e35a17495d3fbb9f9c5ecbdb1c734ae
diodonfrost <diodon.frost@diodonfrost.me>
2019-05-15 22:23:58 +0200

    add inspec tests

---
+++ tests/inspec/terragrunt_spec.rb
@@ -0,0 +1,10 @@
+# Check if Terragrunt is functionnal
+
+control 'terragrunt-01' do
+  impact 1.0
+  title 'Terragrunt install'
+  desc 'Terragrunt should be functionnal'
+  describe command('terragrunt -version') do
+    its('exit_status') { should eq 0 }
+  end
+end
